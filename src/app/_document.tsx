4c34839ec99d27e6eadfc8d572067cee1d59e328
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-08-06 09:53:56 +0100

    added favicon

--- src/app/_document.tsx
+++ src/app/_document.tsx
@@ -23,7 +23,7 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Manrope:wght@200..800&family=Space+Grotesk:wght@300..700&display=swap"
             rel="stylesheet"
           />
-          <link rel="icon" href="/favicon.ico" />
+          <link rel="icon" href="/icons/favicon.ico" />
         </Head>
         <body>
           <Main />
